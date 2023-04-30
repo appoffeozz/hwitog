e71d78b81ab7a5a539be9ca4b96b636e365fea51
anar_guseinov <opofiz1992@gmail.com>
2023-05-01 01:04:37 +0700

    itog

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все
+натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+Console.WriteLine("Введите натуральное число больше 1:");
+int number = int.Parse(Console.ReadLine());
+
+void NumberCounter (int number)
+{
+    if (number < 0) Console.Write($"{number} не натуральное число");
+    if (number == 0) return;
+    Console.Write("{0,4}", number);
+    NumberCounter (number - 1);
+}
+
+NumberCounter(number);
+
+/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.WriteLine("Введите начальное число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число M:");
+int numberN = int.Parse(Console.ReadLine());
+
+
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
+
+GapNumberSum(numberM, numberN, 0);
+
+/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью
+рекурсии. Даны два неотрицательных числа m и n.
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+Console.WriteLine("Введите начальное число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int numberN = int.Parse(Console.ReadLine());
+
+
+int AckermannFunction (int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return AckermannFunction(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
+return AckermannFunction(numberM, numberN);
+}
+
+Console.WriteLine($"Функция Аккермана для чисел A({numberM},{numberN}) = {AckermannFunction(numberM, numberN)}");
